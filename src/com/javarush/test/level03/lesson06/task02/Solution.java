b934453801cb43cca53db87c64ac1bc27d968775
korotkova <y.korotkova@clickberry.com>
2016-04-29 14:10:45 +0300

    lesson 6 update

--- src/com/javarush/test/level03/lesson06/task02/Solution.java
+++ src/com/javarush/test/level03/lesson06/task02/Solution.java
@@ -13,6 +13,16 @@ public class Solution
     public static void main(String[] args)
     {
         //напишите тут ваш код
+        for(int i = 1; i <= 10; i++ ){
+
+            for(int j = 1; j <= 10; j++){
+
+                System.out.print(i*j + " ");
+                if(j==10) System.out.println();
+
+            }
+
+        }
 
     }
 }
